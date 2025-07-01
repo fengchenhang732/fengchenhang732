d6850439e30063d00fc1df11a956482fe934187c
unknown <3067229545@qq.com>
2025-07-01 12:02:15 +0800

    wujunhong V2

--- Untitled2.m
+++ Untitled2.m
@@ -4,7 +4,6 @@ function encoded_msg = rs_encode_mworks(msg, n, k)
     m = 8;                  % GF(2^8), 常用字节级RS码
     t = (n - k) / 2;        % 纠错能力
     prim_poly = 391;        % GF(2^8) 本原多项式 (如 x^8 + x^7 + x^2 + x + 1)
-    
     % 1. 创建伽罗华域
     gf_data = gf(msg, m, prim_poly);
     
